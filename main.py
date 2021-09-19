b801064a1d7c562ec82d21a19ab835bcf134efba
Charl P. Botha <cpbotha@vxlabs.com>
2021-09-19 20:32:48 +0200

    Add ASYNC_DB and sqlite / postgresql switch

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from typing import Optional
 from pathlib import Path
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
-#from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import select, Column, DateTime
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel import create_engine, Field, Session, SQLModel
-#from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+DB = 'postgres'
+ASYNC_DB = False
 
 # we need multiple models so that schema is correctly interpreted between
 # database, POST and GET, each of these sometimes with different optional /
@@ -26,7 +29,9 @@ from sqlmodel import create_engine, Field, Session, SQLModel
 # https://sqlmodel.tiangolo.com/tutorial/fastapi/multiple-models/
 class SampleBase(SQLModel):
     name: str
-    timestamp: datetime
+    # just datetime type by default will get you a timezone-less timestamp
+    # so we have to pass in the Column definition with tz-capable DateTime type
+    timestamp: datetime = Field(sa_column=Column(DateTime(timezone=True)))
     v0: Optional[float] = None
     v1: Optional[float] = None
 
@@ -39,14 +44,21 @@ class SampleCreate(SampleBase):
 class SampleRead(SampleBase):
     id: int
 
-db_path = Path(__file__).parent / "bleh.db"
-# disabling sqlite's same thread check because this is a toy, and I'm only
-# benchmarking reads.
-# https://stackoverflow.com/questions/48218065/programmingerror-sqlite-objects-created-in-a-thread-can-only-be-used-in-that-sa
-# echo=True for developing, False otherwise
-engine = create_engine(f"sqlite:///{db_path}", echo=False, connect_args={"check_same_thread": False})
-# async:
-#engine = create_async_engine(f"sqlite+aiosqlite:///{db_path}", echo=False, connect_args={"check_same_thread": False})
+if DB == 'sqlite':
+    db_path = Path(__file__).parent / "bleh.db"
+    # disabling sqlite's same thread check because this is a toy, and I'm only
+    # benchmarking reads.
+    # https://stackoverflow.com/questions/48218065/programmingerror-sqlite-objects-created-in-a-thread-can-only-be-used-in-that-sa
+    # echo=True for developing, False otherwise
+    if ASYNC_DB:
+        engine = create_async_engine(f"sqlite+aiosqlite:///{db_path}", echo=False, connect_args={"check_same_thread": False})
+    else:
+        engine = create_engine(f"sqlite:///{db_path}", echo=False, connect_args={"check_same_thread": False})
+elif DB == 'postgres':
+    if ASYNC_DB:
+        engine = create_async_engine('postgresql+asyncpg://dbwriter:blehbleh@localhost:5432/dbwriter_python')
+    else:
+        engine = create_engine('postgresql://dbwriter:blehbleh@localhost:5432/dbwriter_python')
 
 # to be used as injected arg "session: Session = Depends(get_session)"
 def get_session():
@@ -55,40 +67,68 @@ def get_session():
 
 app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-    # sync code
-    SQLModel.metadata.create_all(engine)
-    # ASYNC:
-    # async with engine.begin() as conn:
-    #     await conn.run_sync(SQLModel.metadata.create_all)    
-
-
 @app.get("/")
 async def APIRoot():
     """Root of the API"""
     return {"data": "Hello, World!"}
 
-@app.get("/samples", response_model=list[SampleRead])
-def get_samples(session: Session = Depends(get_session)):
-    result = session.execute(select(Sample))
-    samples = result.scalars().all()
-    return samples
+if ASYNC_DB:
+    @app.on_event("startup")
+    async def on_startup():
+        print("ASYNC startup")
+        async with engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)    
+
+    @app.get("/samples", response_model=list[SampleRead])
+    async def get_samples():
+        async with AsyncSession(engine) as session:
+            result = await session.execute(select(Sample))
+            samples = result.scalars().all()
+            return samples
 
 
-@app.post("/samples")
-def add_sample(sample: SampleCreate, session: Session = Depends(get_session)):
-    db_sample = Sample.from_orm(sample)
-    session.add(db_sample)
-    session.commit()
-    session.refresh(db_sample)
-    return db_sample
+    @app.post("/samples")
+    async def add_sample(sample: SampleCreate):
+        db_sample = Sample.from_orm(sample)
+        async with AsyncSession(engine) as session:
+            session.add(db_sample)
+            await session.commit()
+            await session.refresh(db_sample)
+            return db_sample
 
-@app.get("/samples/{sample_id}", response_model=SampleRead)
-def get_sample(sample_id: int, session: Session = Depends(get_session)):
-    # async with AsyncSession(engine) as session:
-        # sample = await session.get(Sample, sample_id)
-    sample = session.get(Sample, sample_id)
-    if sample is None:
-        raise HTTPException(status_code=404, detail=f"sample with id {sample_id} not found")
-    return sample
+    @app.get("/samples/{sample_id}", response_model=SampleRead)
+    async def get_sample(sample_id: int):
+        async with AsyncSession(engine) as session:
+            sample = await session.get(Sample, sample_id)
+            if sample is None:
+                raise HTTPException(status_code=404, detail=f"sample with id {sample_id} not found")
+            return sample
+
+else:
+    @app.on_event("startup")
+    def on_startup():
+        print("SYNC startup")
+        SQLModel.metadata.create_all(engine)
+
+    @app.get("/samples", response_model=list[SampleRead])
+    def get_samples(session: Session = Depends(get_session)):
+        result = session.execute(select(Sample))
+        samples = result.scalars().all()
+        return samples
+
+
+    @app.post("/samples")
+    def add_sample(sample: SampleCreate, session: Session = Depends(get_session)):
+        db_sample = Sample.from_orm(sample)
+        session.add(db_sample)
+        session.commit()
+        session.refresh(db_sample)
+        return db_sample
+
+
+    @app.get("/samples/{sample_id}", response_model=SampleRead)
+    def get_sample(sample_id: int, session: Session = Depends(get_session)):
+        sample = session.get(Sample, sample_id)
+        if sample is None:
+            raise HTTPException(status_code=404, detail=f"sample with id {sample_id} not found")
+        return sample
