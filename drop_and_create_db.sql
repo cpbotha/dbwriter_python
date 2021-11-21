0b80e6e220e27509b86ceabdc34996753b511004
Charl P. Botha <cpbotha@vxlabs.com>
2021-11-21 14:39:08 +0200

    Tweak database setup invocation

--- drop_and_create_db.sql
+++ drop_and_create_db.sql
@@ -1,5 +1,6 @@
 -- as user postgres:
 -- psql < drop_and_create_db.sql
+-- OR if you have it on docker / k8s: psql -h localhost -U postgres < drop_and_create_db.sql
 create user dbwriter with password 'blehbleh';
 drop database dbwriter_python;
 create database dbwriter_python;
