50308f985c9b537366f09d9247d7d71c3907f7ae
Charl P. Botha <cpbotha@vxlabs.com>
2024-07-11 17:06:03 +0200

    Change new database to dbwriter owner
    Required for Postgres 15+

--- drop_and_create_db.sql
+++ drop_and_create_db.sql
@@ -5,3 +5,6 @@ create user dbwriter with password 'blehbleh';
 drop database dbwriter_python;
 create database dbwriter_python;
 grant all privileges on database dbwriter_python to dbwriter;
+-- https://stackoverflow.com/questions/67276391/why-am-i-getting-a-permission-denied-error-for-schema-public-on-pgadmin-4
+-- postgres 15+ you need to make the new user own the new database and/or or grant them all privileges on the public schema within the new database
+ALTER DATABASE dbwriter_python OWNER TO dbwriter;
